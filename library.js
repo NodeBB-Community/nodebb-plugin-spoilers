30eeca2b8283f8e1ee61787fa53ff5fc5e0489c6
VJ T <tedr56@gmail.com>
2014-12-02 20:41:42 +0100

    Fix #3
    Correct hook and data paths
    Update to 0.1.2 for npm

--- library.js
+++ library.js
@@ -2,13 +2,16 @@
 
 var plugin = {};
 
-plugin.parse = function(postContent, callback) {
+plugin.parse = function(data, callback) {
+	if (!data || !data.postData || !data.postData.content) {
+	    return callback(null, data);
+	}
 	// this regex could be better
-	postContent = postContent
+	data.postData.content = data.postData.content
 		.replace(/<p>! *([\S\s]*?)<\/p>/gm, '</blockquote><blockquote class="spoiler"><p>$1</p></blockquote><blockquote>')
 		.replace(/<blockquote>\s*<\/blockquote>/g, '');
 
-	callback(null, postContent);
+	callback(null, data);
 };
 
 module.exports = plugin;
