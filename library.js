d67076b285d178b1744305c8880babd772109039
xusoo <xusoo@users.noreply.github.com>
2015-01-25 18:29:41 +0100

    Few improvements
    - Add parsing to the editor’s preview (filter:parse.raw)
    - Add ability to set a title for the spoiler
    (https://i.imgur.com/NP4GSTT.png)
    - Add 0.5s of delay before showing the spoiler to avoid viewing it by
    accident

--- library.js
+++ library.js
@@ -6,12 +6,18 @@ plugin.parse = function(data, callback) {
 	if (!data || !data.postData || !data.postData.content) {
 	    return callback(null, data);
 	}
-	// this regex could be better
-	data.postData.content = data.postData.content
-		.replace(/<p>! *([\S\s]*?)<\/p>/gm, '</blockquote><blockquote class="spoiler"><p>$1</p></blockquote><blockquote>')
-		.replace(/<blockquote>\s*<\/blockquote>/g, '');
 
-	callback(null, data);
+	plugin.parseRaw(data.postData.content, function (err, content) {
+		data.postData.content = content;
+		callback(err, data);
+	});
+};
+
+plugin.parseRaw = function (content, callback) {
+	callback(null, 
+		content.replace(/<p>! *(\((.+?)\))?([\S\s]*?)<\/p>/gm, '</blockquote><blockquote class="spoiler" data-title="$2"><p>$3</p></blockquote><blockquote>')
+		.replace(/<blockquote>\s*<\/blockquote>/g, '')
+	);
 };
 
 module.exports = plugin;
