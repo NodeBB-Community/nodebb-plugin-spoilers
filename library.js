12f8206008401b309db480f3a0e076d230bd4d81
A Catty Alpaca <Alpacatty@users.noreply.github.com>
2015-04-17 00:13:26 +0200

    rewrite?

--- library.js
+++ library.js
@@ -2,15 +2,27 @@
 
 var plugin = {};
 
-plugin.parse = function(data, callback) {
-	if (!data || !data.postData || !data.postData.content) {
-	    return callback(null, data);
-	}
-	// this regex could be better
-	data.postData.content = data.postData.content
-		.replace(/<p>! *([\S\s]*?)<\/p>/gm, '</blockquote><blockquote class="spoiler"><p>$1</p></blockquote><blockquote>')
-		.replace(/<blockquote>\s*<\/blockquote>/g, '');
+function spoilerify(content) {
+	content = content.replace(/<blockquote>\n*<p>!([\s\S]*?)<\/p>\n*<\/blockquote>/gi, '<blockquote class="spoiler"><p>$1</p></blockquote>');
 
+	return content;
+};
+
+plugin.post = function(data, callback) {
+	if (!data || !data.postData || !data.postData.content) {
+		return callback(null, data);
+	}
+
+	data.postData.content = spoilerify(data.postData.content);
+	callback(null, data);
+};
+
+plugin.raw = function(data, callback) {
+	if (!data) {
+		return callback(null, data)
+	}
+
+	data = spoilerify(data);
 	callback(null, data);
 };
 
